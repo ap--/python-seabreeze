b90675598941b0f6640a20dc4288faa759e27e5c
ap-- <mail@andreaspoehlmann.de>
2015-10-12 12:48:12 +0200

    Update misc/install_libseabreeze.sh

--- misc/install_libseabreeze.sh
+++ misc/install_libseabreeze.sh
@@ -4,6 +4,21 @@ SEABREEZE_FILE="seabreeze-3.0.10.zip"
 SEABREEZE_FOLDER=$(basename $SEABREEZE_FILE .zip)
 SEABREEZE_URL="http://sourceforge.net/projects/seabreeze/files/SeaBreeze/source"
 
+case "$(uname -s)" in
+  Linux)
+    SEABREEZE_EXTENSION="so"
+    ;;
+  Darwin)
+    SEABREEZE_EXTENSION="dylib"
+    ;;
+  CYGWIN*)
+    SEABREEZE_EXTENSION="dll"
+    ;;
+  *)
+    echo 'Your operating system is not supported.'
+    ;;
+esac
+
 if [ ! -f $SEABREEZE_FILE ]; then
     echo -e "# SEABREEZE: downloading $SEABREEZE_FILE"
     wget -nv -nc "$SEABREEZE_URL/$SEABREEZE_FILE"
@@ -18,8 +33,8 @@ fi
 
 echo -e "# SEABREEZE: building shared library"
 cd $SEABREEZE_FOLDER
-make lib/libseabreeze.so
+make "lib/libseabreeze.$SEABREEZE_EXTENSION"
 cd ..
 
 echo -e "# SEABREEZE: installing to /usr/local/lib"
-sudo install "$SEABREEZE_FOLDER/lib/libseabreeze.so" /usr/local/lib/
+sudo install "$SEABREEZE_FOLDER/lib/libseabreeze.$SEABREEZE_EXTENSION" /usr/local/lib/

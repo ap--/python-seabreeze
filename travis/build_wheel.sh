899905a03d572d7be36d219273a9571f429a03b5
ap-- <mail@andreaspoehlmann.de>
2016-02-15 20:25:28 +0100

    [travis] use python.org python interpreters
    Closes #19
    
    * python.org python interpreters are fat binary builds and support
      building wheels for --arch=intel
    * includes building wheels for python 3.5

--- travis/build_wheel.sh
+++ travis/build_wheel.sh
@@ -4,28 +4,19 @@ wget https://github.com/ap--/libseabreeze/releases/download/libseabreeze-${SEABR
 hdiutil attach libseabreeze-${SEABREEZE_VERSION}.dmg
 cp /Volumes/lib/libseabreeze.dylib /usr/local/lib
 
-# Install miniconda 
-# comment this block out for local builds.
-wget http://repo.continuum.io/miniconda/Miniconda-3.5.2-MacOSX-x86_64.sh \
-    -O miniconda.sh
-chmod +x miniconda.sh
-./miniconda.sh -b
-export PATH=/Users/travis/miniconda/bin:$PATH
-conda update --yes conda
-
-# create a conda environment with the necessary Python modules for a build
-conda create --yes -n build_env python=$PYTHON_VERSION
-source activate build_env
-conda install --yes setuptools pip numpy=$NUMPY_VERSION six cython
+# Setup python environment
+git clone https://github.com/MacPython/terryfy
+source terryfy/travis_tools.sh
+get_python_environment macpython $PYTHON_VERSION venv
+pip install --upgrade pip
+pip install --upgrade wheel
+pip install cython
+pip install numpy==$NUMPY_VERSION
+pip install delocate
 python --version
 
 # build the wheel
 echo "Building seabreeze-wheel for Python: $PYTHON_VERSION"
-pip install --upgrade wheel
-pip install --upgrade delocate
-ARCHFLAGS="-arch i386 -arch x86_64" python setup.py bdist_wheel
+python setup.py bdist_wheel
 wfn=$(echo dist/*.whl)
 delocate-wheel --require-archs=intel -v $wfn
-
-# clean up
-source deactivate

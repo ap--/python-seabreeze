bcaf8d19718028303d3195d2677c76f9b3ecd158
Weiqiang Li <liweiqiang723@gmail.com>
2017-03-19 09:45:34 +0800

    spectrometers.py
    Add: 
    ++++++++++++++++++++++++++++++++++
    self._fidspp = feature.add('spectrum_processing') 
    ++++++++++++++++++++++++++++++++++
    def boxcar_width(self, boxcar_width):
            lib.spectrometer_set_boxcar_width(self._dev, self._fidspp, boxcar_width)
    def scans_to_average(self, scans_to_average):
            lib.spectrometer_set_scans_to_average(self._dev, self._fidspp, scans_to_average)
    ++++++++++++++++++++++++++++++++++

--- seabreeze/spectrometers.py
+++ seabreeze/spectrometers.py
@@ -103,6 +103,7 @@ class Spectrometer(object):
         self._fidte = feature.add('tec')
         self._fidnc = feature.add('nonlinearity_coeffs')  # Added
         self._fidsl = feature.add('stray_light_coeffs')
+        self._fidspp = feature.add('spectrum_processing')
         # get additional information
         self._pixels = lib.spectrometer_get_formatted_spectrum_length(self._dev, self._fidsp)
         self._minimum_integration_time_micros = (
@@ -167,8 +168,12 @@ class Spectrometer(object):
         lib.spectrometer_set_integration_time_micros(self._dev,
                                                      self._fidsp, integration_time_micros)
 
-    def trigger_mode(self, mode):
-        lib.spectrometer_set_trigger_mode(self._dev, self._fidsp, mode)
+    def boxcar_width(self, boxcar_width):
+        lib.spectrometer_set_boxcar_width(self._dev, self._fidspp, boxcar_width)
+
+    def scans_to_average(self, scans_to_average):
+        lib.spectrometer_set_scans_to_average(self._dev, self._fidspp, scans_to_average)
+
 
     @property
     def serial_number(self):

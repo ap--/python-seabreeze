459095b49b2669c8f67adf156811515b94ee3ffe
ap-- <andreas@poehlmann.io>
2019-06-19 16:52:38 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,63 +1,74 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
-from distutils.extension import Extension
-from distutils.version import LooseVersion
+import os.path
 import platform
 import sys
-import warnings
+
+from setuptools import setup, Extension, find_packages
 
 SEABREEZE_VERSION = "0.6.0"
-
 
 if "--without-cseabreeze" in sys.argv:
     sys.argv.remove("--without-cseabreeze")  # this is a hack...
     # user requests to not install cython wrapper
     _extensions = []
 else:
-    # default to building the cython wrapper
-    try:
-        # try to import cython
-        import Cython
-        # We require at least version 0.18
-        if LooseVersion(Cython.__version__) < LooseVersion("0.18"):
-            raise ImportError("Cython version < 0.18")
-    except ImportError:
-        # if not installed or too old fall back to the provided C file
-        cythonize = lambda x: x
-        fn_ext = "c"
-    else:
-        from Cython.Build import cythonize
-        fn_ext = "pyx"
+    system = platform.system()
 
-    # The windows version of the cython wrapper depends on winusb
+    # cseabreeze source files
+    sources = ['src/seabreeze/cseabreeze/wrapper.pyx']
+    for root, dirnames, filenames in os.walk('src/libseabreeze'):
+        for filename in filenames:
+            if os.path.splitext(filename)[1] in {'.c', '.cpp'}:
+                if 'native' in root:
+                    if root.endswith("windows") and system != 'Windows':
+                        continue
+                    elif root.endswith("winusb") and system != 'Windows':
+                        continue
+                    elif root.endswith("linux") and system in {'Darwin', 'Windows'}:
+                        continue
+                    elif root.endswith("osx") and system != 'Darwin':
+                        continue
+                    elif root.endswith("posix") and system == 'Windows':
+                        continue
+                sources.append(os.path.join(root, filename))
+
+    # link
     if platform.system() == "Windows":
-        libs = ['seabreeze', 'winusb']
+        libs = ['winusb']
     elif platform.system() == "Darwin":
-        libs = ['seabreeze']
+        libs = []
     else:
-        libs = ['seabreeze', 'usb']
-
+        libs = ['usb']
     # define extension
-    extensions = [Extension('seabreeze.cseabreeze.wrapper',
-                        ['./seabreeze/cseabreeze/wrapper.%s' % fn_ext],
-                        libraries=libs,
-                      )]
-    _extensions = cythonize(extensions)
+    libseabreeze = Extension('seabreeze.cseabreeze.wrapper',
+                             sources=sources,
+                             include_dirs=['src/libseabreeze/include'],
+                             libraries=libs)
+    _extensions = [libseabreeze]
 
 setup(
     name='seabreeze',
     version=SEABREEZE_VERSION,
     author='Andreas Poehlmann',
-    author_email='mail@andreaspoehlmann.de',
-    packages=['seabreeze',
-              'seabreeze.cseabreeze',
-              'seabreeze.pyseabreeze',
-              'seabreeze.pyseabreeze.interfaces'],
-    scripts=['scripts/seabreeze-compare'],
+    author_email='andreas@poehlmann.io',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    setup_requires=[
+        # Setuptools 18.0 properly handles Cython extensions.
+        'setuptools>=18.0',
+        'cython>=0.18',
+        'numpy'
+    ],
+    scripts=[
+        'scripts/seabreeze-compare'
+    ],
     description=('Python interface module for oceanoptics spectrometers. '
                  'This software is not associated with Ocean Optics. '
                  'Use it at your own risk.'),
     long_description=open('README.md').read(),
-    requires=['python (>= 2.7)', 'pyusb (>= 1.0)', 'numpy'],
+    install_requires=[
+        'pyusb>=1.0',
+        'numpy'
+    ],
     ext_modules=_extensions,
 )

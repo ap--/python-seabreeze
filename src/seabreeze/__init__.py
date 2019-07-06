36acdb967d83cae26954823e530e3f54d0a3b068
ap-- <andreas@poehlmann.io>
2019-07-06 16:54:06 +0200

    seabreeze module docstring

--- src/seabreeze/__init__.py
+++ src/seabreeze/__init__.py
@@ -1,4 +1,56 @@
+"""
+SeaBreeze
+=========
 
+Python module for accessing Ocean Optics Spectrometers.
+Returns spectra as NumPy Arrays and unifies the spectrometer
+control for all supported spectrometer models.
+
+How to use the module
+---------------------
+To measure a spectrum do::
+
+  >>> import seabreeze
+  >>> seabreeze.use("cseabreeze")
+  >>> from seabreeze.spectrometers import Spectrometer
+  >>> spec = Spectrometer.from_serial_number(serial=None)  # serial=None picks the first available
+  >>> wavelengths = spec.wavelengths()
+  >>> intensities = spec.intensities()
+
+Available subpackages
+---------------------
+spectrometers
+    Core interface to the spectrometers
+backend
+    internal backend loading functions
+cseabreeze
+    libseabreeze Cython wrapped backend
+pyseabreeze
+    pyusb implementation of the cseabreeze backend
+
+Utilities
+---------
+utils
+    -
+system
+    operating system related functionality
+
+Operating System dependent setup
+--------------------------------
+Dependent on your operating system there's steps you have to take to make
+the spectrometer visible to python-seabreeze.
+
+Linux
+    requires to install the udev rules
+Osx
+    should work ootb
+Windows
+    requires installing the drivers
+
+"""
+# noinspection PyUnresolvedReferences
 from seabreeze.backends import use
 
 __version__ = "0.6.0"
+__author__ = "Andreas Poehlmann"
+__email__ = "andreas@poehlmann.io"

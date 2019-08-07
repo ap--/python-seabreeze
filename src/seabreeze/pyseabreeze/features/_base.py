515dfc5ea08e85e29ec3b9f1764ca6e7abe8a816
ap-- <andreas@poehlmann.io>
2019-08-07 21:48:18 +0200

    pyseabreeze.features._base update baseclass

--- src/seabreeze/pyseabreeze/features/_base.py
+++ src/seabreeze/pyseabreeze/features/_base.py
@@ -1,37 +1,41 @@
 
 from seabreeze.pyseabreeze.exceptions import SeaBreezeError
-from seabreeze.pyseabreeze.communication import USBCommBase
+from seabreeze.pyseabreeze.protocol import ProtocolInterface
 
 
 class SeaBreezeFeature(object):
 
     identifier = "base_feature"
-    required = False
-    required_interface_cls = USBCommBase
-    required_features = ()
 
-    def __init__(self, device, feature_id):
+    _required_kwargs = ()
+    _required_features = ()
+    _required_protocol_cls = ProtocolInterface
+
+    def __init__(self, protocol, feature_id, **kwargs):
         """SeaBreezeFeature base class
 
         Parameters
         ----------
-        device : seabreeze.pyseabreeze.devices.SeaBreezeDevice
+        protocol : seabreeze.pyseabreeze.protocol.ProtocolInterface
         feature_id : int
         """
         if self.identifier == "base_feature":
             raise SeaBreezeError("Don't instantiate SeaBreezeFeature directly. Use derived feature classes.")
-        if not isinstance(device, self.required_interface_cls):
-            raise SeaBreezeError("Feature can't be instantiated for device. Device not instance of {}".format(
-                str(self.required_interface_cls)
-            ))
-        self.device = device
+        assert set(self._required_kwargs) == set(kwargs)
+        # check protocol support
+        if not isinstance(protocol, self._required_protocol_cls):
+            raise SeaBreezeError("FeatureError: Protocol not supported by feature")
+        self.protocol = protocol
         self.feature_id = feature_id
 
     def __repr__(self):
-        return "<{}:{}:{} id={}>".format(self.__class__.__name__,
-                                         self.device.model, self.device.serial_number, self.feature_id)
+        return "<{} id={}>".format(self.__class__.__name__, self.feature_id)
 
     @classmethod
     def get_feature_class_registry(cls):
         # noinspection PyUnresolvedReferences
         return {feature_class.identifier: feature_class for feature_class in SeaBreezeFeature.__subclasses__()}
+
+    @classmethod
+    def supports_protocol(cls, protocol):
+        return isinstance(protocol, cls._required_protocol_cls)

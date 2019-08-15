6e7fb88e375a55518ea49ede17dc7ee0ab19294a
ap-- <andreas@poehlmann.io>
2019-08-15 11:27:14 +0200

    pyseabreeze.features.rawusb update after refactor

--- src/seabreeze/pyseabreeze/features/rawusb.py
+++ src/seabreeze/pyseabreeze/features/rawusb.py
@@ -1,22 +1,29 @@
-from seabreeze.pyseabreeze.communication import USBCommBase
+from seabreeze.pyseabreeze.protocol import ProtocolInterface
+from seabreeze.pyseabreeze.transport import USBTransport
 from seabreeze.pyseabreeze.features._base import SeaBreezeFeature
 
 
 class SeaBreezeRawUSBAccessFeature(SeaBreezeFeature):
     identifier = 'raw_usb_bus_access'
-    required_interface_cls = USBCommBase
+    _required_protocol_cls = ProtocolInterface
+
+    @classmethod
+    def supports_protocol(cls, protocol):
+        return isinstance(protocol.transport, USBTransport)
 
     def raw_usb_write(self, data, endpoint):
         if endpoint not in {'primary_out', 'secondary_out'}:
             raise ValueError("endpoint has to be one of 'primary_out', 'secondary_out'")
-        self.device.usb_send(data)
+        if endpoint == 'secondary_out':
+            pass
+        self.protocol.transport.write(data)
 
     def raw_usb_read(self, endpoint, buffer_length=None):
         if endpoint == 'primary_in':
-            return self.device.usb_read_lowspeed(size=buffer_length or 64)
+            return self.protocol.transport.read(size=buffer_length, mode='low_speed')
         elif endpoint == 'secondary_in':
-            return self.device.usb_read_highspeed(size=buffer_length or 512)
+            return self.protocol.transport.read(size=buffer_length, mode='high_speed')
         elif endpoint == 'secondary_in2':
-            return self.device.usb_read_highspeed_alt(size=buffer_length or 512)
+            return self.protocol.transport.read(size=buffer_length, mode='high_speed_alt')
         else:
             raise ValueError("endpoint has to be one of 'primary_in', 'secondary_in', 'secondary_in2'")

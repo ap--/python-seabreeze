d2e9cb85fd473969fb66cd15ac8a280032616a2c
ap-- <andreas@poehlmann.io>
2019-08-15 11:29:10 +0200

    pyseabreeze.features.thermoelectric update after refactor

--- src/seabreeze/pyseabreeze/features/thermoelectric.py
+++ src/seabreeze/pyseabreeze/features/thermoelectric.py
@@ -1,6 +1,6 @@
 import struct
 
-from seabreeze.pyseabreeze.communication import USBCommOOI, USBCommOBP
+from seabreeze.pyseabreeze.protocol import OOIProtocol, OBPProtocol
 from seabreeze.pyseabreeze.features._base import SeaBreezeFeature
 
 
@@ -24,17 +24,17 @@ class SeaBreezeThermoElectricFeature(SeaBreezeFeature):
 # ==================
 #
 class ThermoElectricFeatureOOI(SeaBreezeThermoElectricFeature):
-    required_interface_cls = USBCommOOI
+    _required_protocol_cls = OOIProtocol
 
     def enable_tec(self, state):
-        self.device.usb_send(struct.pack('<BBB', 0x71, bool(state), 0x00))
+        self.protocol.send(0x71, (bool(state), 0x00))
 
     def set_temperature_setpoint_degrees_celsius(self, temperature):
-        self.device.usb_send(struct.pack('<Bh', 0x73, int(temperature * 10)))
+        self.protocol.send(0x73, temperature * 10)
 
     def read_temperature_degrees_celsius(self):
-        self.device.usb_send(struct.pack('<B', 0x72))
-        ret = self.device.usb_read_lowspeed(timeout=1000)
+        self.protocol.send(0x72)
+        ret = self.protocol.receive(timeout_ms=1000, mode='low_speed')
         return struct.unpack('<h', ret)[0] / 10.0
 
     # NOTE: if implemented this would also need to be added to the interface
@@ -48,16 +48,16 @@ class ThermoElectricFeatureOOI(SeaBreezeThermoElectricFeature):
 # ==================
 #
 class ThermoElectricFeatureOBP(SeaBreezeThermoElectricFeature):
-    required_interface_cls = USBCommOBP
+    _required_protocol_cls = OBPProtocol
 
     def enable_tec(self, state):
-        self.device.send_command(0x00420010, struct.pack("<B", bool(state)))
+        self.protocol.send(0x00420010, bool(state))
 
     def set_temperature_setpoint_degrees_celsius(self, temperature):
-        self.device.send_command(0x00420011, struct.pack("<f", float(temperature)))
+        self.protocol.send(0x00420011, float(temperature))
 
     def read_temperature_degrees_celsius(self):
-        ret = self.device.query(0x00420004, "")
+        ret = self.protocol.query(0x00420004)
         return struct.unpack("<f", ret)[0]
 
     # def set_tec_fan_enable(self, tec_fan_enable):

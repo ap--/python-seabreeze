8727d3ff0496cc00f1de4cad755294ce4e1dedeb
ap-- <andreas@poehlmann.io>
2019-08-27 18:58:18 +0200

    ci: windows builds

--- travis/build_windows_wheels.sh
+++ travis/build_windows_wheels.sh
@@ -3,9 +3,8 @@ echo "Building wheel"
 python --version
 pip --version
 
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64 $SBBUILDSDK
+echo "# INCLUDE:"
+echo $INCLUDE
 
-export DISTUTILS_USE_SDK=1
-export MSSdk=1
-
+echo "# "
 pip wheel . -w wheelhouse/

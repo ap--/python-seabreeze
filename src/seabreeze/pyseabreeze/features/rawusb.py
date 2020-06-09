d20c27b8c1ba4ecd344c3c61e9926deddbb4f7c7
ap-- <andreas@poehlmann.io>
2020-06-09 09:06:16 +0200

    [docs] pyseabreeze: add example to raw usb access feature
    Close #85.

--- src/seabreeze/pyseabreeze/features/rawusb.py
+++ src/seabreeze/pyseabreeze/features/rawusb.py
@@ -4,6 +4,30 @@ from seabreeze.pyseabreeze.transport import USBTransport
 
 
 class SeaBreezeRawUSBBusAccessFeature(SeaBreezeFeature):
+    r"""
+    Reading and writing raw usb
+
+    Example usage
+
+    .. code-block:: python
+
+     >>> import struct  # needed for packing binary data into bytestrings
+     >>> from seabreeze.spectrometers import Spectrometer
+     >>> spec = Spectrometer.from_first_available()
+     # features need to be accessed on the spectrometer instance via spec.f or spec.features
+     # the data you provide needs to packed into a bytestring via struct.pack
+     >>> spec.f.raw_usb_bus_access.raw_usb_write(data=struct.pack('<B', 0xFE), endpoint='primary_out')
+
+     # when reading via the raw_usb feature you can easily block the spectrometer.
+     # so make sure to only read when you expect new data (after you sent a command)
+     # and only read as many bytes as the command returns
+     >>> output = spec.f.raw_usb_bus_access.raw_usb_read(endpoint='primary_in', buffer_length=16)
+
+     >>> print(output)
+     b'\x00\x08p\x17\x00\x00\x00\x00\x00\x08\x00\x00\x00\x00\x80U'
+     # extract by providing the data format from the developer datasheet and `output` to struct.unpack
+
+    """
     identifier = "raw_usb_bus_access"
     _required_protocol_cls = ProtocolInterface
 

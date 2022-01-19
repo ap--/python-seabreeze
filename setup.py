797d4a97b108e6cd559ea3ef4f8a8f3779d70c4f
Andreas Poehlmann <andreas@poehlmann.io>
2022-01-19 19:06:52 +0100

    [seabreeze] set cython language_level 3

--- setup.py
+++ setup.py
@@ -125,6 +125,7 @@ else:
     libseabreeze.cython_directives = {
         "binding": building_sphinx_documentation,  # fix class method parameters for sphinx
         "embedsignature": not building_sphinx_documentation,  # add function signature to docstring for ipython
+        "language_level": "3",  # prevent cython warning
     }
     extensions = [libseabreeze]
 

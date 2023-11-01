ff852cde3bd886cfdf52f748b93e5182a16dd8c6
Andreas Poehlmann <ap--@users.noreply.github.com>
2023-11-01 16:56:54 +0100

    add sr2 support
    Closes #217.
    
    Authored-by: @avedillopy

--- src/seabreeze/pyseabreeze/devices.py
+++ src/seabreeze/pyseabreeze/devices.py
@@ -1202,6 +1202,30 @@ class ADC1000USB(SeaBreezeDevice):
     )
 
 
+class SR2(SeaBreezeDevice):
+    model_name = "SR2"
+
+    # communication config
+    transport = (USBTransport,)
+    usb_vendor_id = 0x0999
+    usb_product_id = 0x1001
+    usb_endpoint_map = EndPointMap(ep_out=0x01, highspeed_in=0x81)
+    usb_protocol = OBP2Protocol
+
+    # spectrometer config
+    dark_pixel_indices = DarkPixelIndices.from_ranges((0, 24))
+    integration_time_min = 1  # 1 us
+    integration_time_max = 2000000  # 2 s
+    integration_time_base = 1
+    spectrum_num_pixel = 2110
+    spectrum_raw_length = (2110 * 2) + 32  # Metadata
+    spectrum_max_value = 65535
+    trigger_modes = TriggerMode.supported("OBP_NORMAL")
+
+    # features
+    feature_classes = (sbf.spectrometer.SeaBreezeSpectrometerFeatureSR2,)
+
+
 class SR4(SeaBreezeDevice):
     model_name = "SR4"
 

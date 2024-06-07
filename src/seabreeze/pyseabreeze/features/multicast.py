249836f00df101da407d50a1ceb5a220654b1c4e
Andreas Poehlmann <ap--@users.noreply.github.com>
2024-06-07 22:39:14 +0200

    psb: remove extra MulticastFeature methods for now

--- src/seabreeze/pyseabreeze/features/multicast.py
+++ src/seabreeze/pyseabreeze/features/multicast.py
@@ -1,5 +1,4 @@
 import struct
-from typing import List
 
 from seabreeze.pyseabreeze.features._base import SeaBreezeFeature
 from seabreeze.pyseabreeze.protocol import OBPProtocol
@@ -21,15 +20,6 @@ class SeaBreezeMulticastFeature(SeaBreezeFeature):
     ) -> None:
         raise NotImplementedError("implement in derived class")
 
-    def get_multicast_group_address(self, interface_index: int) -> List[int]:
-        raise NotImplementedError("implement in derived class")
-
-    def get_multicast_group_port(self, interface_index: int) -> int:
-        raise NotImplementedError("implement in derived class")
-
-    def get_multicast_ttl(self, interface_index: int) -> int:
-        raise NotImplementedError("implement in derived class")
-
 
 # OBP implementation
 # ==================
@@ -45,16 +35,3 @@ class SeaBreezeMulticastFeatureOBP(SeaBreezeMulticastFeature):
         self, interface_index: int, enable_state: bool
     ) -> None:
         raise NotImplementedError("missing from HDX FW documentation")
-
-    def get_multicast_group_address(self, interface_index: int) -> List[int]:
-        ret = self.protocol.query(0x00000A81, int(interface_index))
-        ip = struct.unpack("<BBBB", ret)
-        return [int(ip[0]), int(ip[1]), int(ip[2]), int(ip[3])]
-
-    def get_multicast_group_port(self, interface_index: int) -> int:
-        ret = self.protocol.query(0x00000A82, int(interface_index))
-        return int(struct.unpack("<H", ret)[0])
-
-    def get_multicast_ttl(self, interface_index: int) -> int:
-        ret = self.protocol.query(0x00000A83, int(interface_index))
-        return int(struct.unpack("<B", ret)[0])
